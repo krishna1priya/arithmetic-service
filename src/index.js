d44895f1eb65b29884e1b20b4e95998720e82ae5
krishna1priya <krishnapriya051@gmail.com>
2024-02-15 19:33:54 -0500

    adding tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const {add} = require("./arithmetica");
 const app = express();
 app.use(cors());
 const port = 3000;
@@ -9,7 +10,8 @@ const port = 3000;
 // });
 
 app.get('/add/:n/:m',(req,res)=>{
-    res.json(Number(req.params.n) + Number(req.params.m));
+    let sum = add(Number(req.params.n), Number(req.params.m));
+    res.json(sum);
 });
 
 app.listen(port);
